62c84ae8de582579a988726cab147ddbfb4264b5
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2018-09-12 21:06:34 +0200

    Initial setup for hub3 main structures and interfaces.

--- hub3/main.go
+++ hub3/main.go
@@ -2,7 +2,7 @@ package hub3
 
 import "fmt"
 
-// Deleter
+// Deleter is a generic interface for 'deleting' from the storage layer
 type Deleter interface {
 	Delete() error
 }
@@ -10,7 +10,7 @@ type Deleter interface {
 // BulkProcessor is a generic interface to 'add' to the storage layer
 // todo later change this for generic with FragmentGraph
 type BulkProcessor interface {
-	Add(request BulkableRequest) error
+	Add(record Record) error
 }
 
 // BulkableRequest is a generic interface to bulkable requests.
@@ -18,3 +18,54 @@ type BulkableRequest interface {
 	fmt.Stringer
 	Source() ([]string, error)
 }
+
+type HubConfig struct{}
+
+// Tenant is the top data structure for the Hub3
+// A Hub3  instance holds one or more Tenant tenants
+// All Tenants are read-write and need to be created via the API
+// Their information is stored in the Configuration storage
+type Tenant struct {
+	OrgID string
+	//Collections []Collection
+	//Config      TenantConfig
+}
+
+type Collection struct {
+	Tenant
+	Spec         string
+	Revision     int64
+	ErrorMessage string
+	//Config   CollectionConfig
+	//SourceType
+}
+
+type Record struct {
+}
+
+type Resource struct {
+}
+
+type Entry struct {
+}
+
+type RecordStore interface {
+	Add(record Record)
+	Drop(hubId string)
+	DropOrphans(spec string)
+	DropAll(spec string)
+}
+
+type QuadStore interface {
+	Add(record Record)
+	AddTriple(triple string, context string)
+	Drop(hubId string)
+	DropTriple(hubId string)
+	DropGraph(context string)
+	DropOrphans(spec string)
+	DropAll(spec string)
+}
+
+/*
+
+ */
